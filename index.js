92ee12c349470f63f458b122bfc2adfb964ef610
Suleiman Mamman <suleigolden>
2021-03-07 22:00:59 +0100

    Create a subscription endpoint edited

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/subscribe', userRouter);
-app.get('/', (req, res) => {
-    //console.log(req);
-    res.send('<h1>Hello</h1>')
-});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server running on ... port ' + PORT));
--- routes/subscriber.js
+++ routes/subscriber.js
@@ -4,19 +4,14 @@ const router = express.Router();
 //tem data as 
 const notification = [];
 
-router.get('/', (req, res) => {
-     console.log('Hello from subscriber route');
-     res.send('<h1>Hello from subscriber route</h1>')
-});
-
 //Publisher Server Endpoint
-router.post('/', (req, res) => {
+router.post('/topic1', (req, res) => {
      res.setHeader('Content-Type', 'application/json');
      const notify = req.body;
      if(notify){
           console.log(notify);
           notification.push(notify);
-          res.end(JSON.stringify({ topic: notify.topic, url: notify.body, }));
+          res.end(JSON.stringify({ topic: notify.topic, url: notify.data.msg, }));
      }else{
           res.end(JSON.stringify({ error: 500 }));
      }
