5f8392a8bf43a6088d592322196f06aff6cf2215
Suleiman Mamman <suleigolden>
2021-03-07 17:36:44 +0100

    Publisher Server Endpoint created

--- routes/subscriber.js
+++ routes/subscriber.js
@@ -1,11 +1,20 @@
 import express from 'express';
 const router = express.Router();
 
+//tem data as 
+const notification = [];
 
 router.get('/', (req, res) => {
      console.log('Hello from subscriber route');
      res.send('<h1>Hello from subscriber route</h1>')
 });
+
+//Publisher Server Endpoint
+router.post('/', (req, res) => {
+     const notify = req.body;
+     notification.push(notify);
+     res.send(`notification  added successfuly.. ${user.message}`);
+ });
 
 
 
